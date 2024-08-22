8f3945e1f5defc35edf8af703085921b3042a132
Gorush116 <gorush34@gmail.com>
2024-08-22 10:58:10 +0900

    common response refactor

--- src/main/java/com/clp/stexample/common/response/CommonResponse.java
+++ src/main/java/com/clp/stexample/common/response/CommonResponse.java
@@ -1,21 +1,20 @@
 package com.clp.stexample.common.response;
 
 import lombok.Getter;
-import lombok.experimental.SuperBuilder;
 
-import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 @Getter
-@SuperBuilder
 public abstract class CommonResponse {
     private final String status;
     private final String message;
-    private final LocalDateTime timestamp;
+    private final ZonedDateTime timestamp;
 
     public CommonResponse(String status, String message) {
         this.status = status;
         this.message = message;
-        this.timestamp = LocalDateTime.now();
+        this.timestamp = ZonedDateTime.now(ZoneId.systemDefault());
     }
 
 }
