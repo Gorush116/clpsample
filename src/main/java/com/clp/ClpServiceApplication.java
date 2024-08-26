9b40eebe3b009d1ed175cceb47eb4960cb90c96c
Gorush116 <gorush34@gmail.com>
2024-08-26 09:45:09 +0900

    디렉토리 구조 변경

--- src/main/java/com/clp/ClpServiceApplication.java
+++ src/main/java/com/clp/ClpServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StexampleApplication {
+public class ClpServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StexampleApplication.class, args);
+        SpringApplication.run(ClpServiceApplication.class, args);
     }
 
 }
