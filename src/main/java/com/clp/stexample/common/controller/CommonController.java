6a45a6892191e00cc473d36822c4d21f3be8af90
DESKTOP-FNUCQHD\user <namjun88@gmail.com>
2024-08-26 09:46:01 +0900

    pom.xml CommonController 업데이트 반영

--- src/main/java/com/clp/stexample/common/controller/CommonController.java
+++ src/main/java/com/clp/stexample/common/controller/CommonController.java
@@ -1,8 +1,11 @@
 package com.clp.stexample.common.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.clp.stexample.common.service.UserService;
 
 import io.swagger.v3.oas.annotations.Operation;
 
@@ -21,5 +24,17 @@ public class CommonController {
     @GetMapping("/login")
     public String loginCheck() {
         return "Login";
+    }
+    
+    @Autowired
+    private UserService userService;
+    
+    
+    @Operation(summary = "test", description = "test")
+    @GetMapping("/test/con")
+    @ResponseBody
+    public String testCon() {
+    	userService.findAll();
+        return "Hello";
     }
 }
